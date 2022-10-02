a46b913cda03220f006673b1c075531b0c5896e2
Monayem Islam <monayem.dev@gmail.com>
2022-10-02 17:19:41 +0600

    Dashboard Template Inserted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftSidebarComponent } from './common/template-parts/left-sidebar/left-sidebar.component';
+import { TopbarComponent } from './common/template-parts/topbar/topbar.component';
+import { OverviewCardsComponent } from './common/template-parts/overview-cards/overview-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftSidebarComponent,
+    TopbarComponent,
+    OverviewCardsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/common/template-parts/overview-cards/overview-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overview-cards',
+  templateUrl: './overview-cards.component.html',
+  styleUrls: ['./overview-cards.component.css']
+})
+export class OverviewCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
